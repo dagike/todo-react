06265f5a0603fe941eb6a7b5e299fdae7a2b0dce
DAGIKE <isaac20053@hotmail.com>
2019-09-11 16:26:51 -0400

    add contexts to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "./layout/Header";
 import Content from "./layout/Content";
+import { ProjectsProvider, SelectedProjectProvider } from "../context";
 import "./App.scss";
 
 export default () => (
-  <div className="App">
-    <Header />
-    <Content />
-  </div>
+  <SelectedProjectProvider>
+    <ProjectsProvider>
+      <div className="App">
+        <Header />
+        <Content />
+      </div>
+    </ProjectsProvider>
+  </SelectedProjectProvider>
 );
