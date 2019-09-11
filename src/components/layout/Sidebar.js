1ab3993c6112182e562249897d01d63269f79710
DAGIKE <isaac20053@hotmail.com>
2019-09-11 17:06:07 -0400

    add ProjectList component to Sidebar

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,40 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   FaChevronDown,
   FaInbox,
   FaRegCalendarAlt,
   FaRegCalendar
 } from "react-icons/fa";
+import { useSelectedProjectValue } from "../../context";
+import Projects from "../ProjectList";
 
-export default () => (
-  <div className="sidebar" data-testid="sidebar">
-    <ul className="sidebar__generic">
-      <li className="inbox" data-testid="inbox">
+export default () => {
+  const { setSelectedProject } = useSelectedProjectValue;
+  const [active, setActive] = useState("INBOX");
+  const [showProjects, setShowProjects] = useState(true);
+
+  return (
+    <div className="sidebar" data-testid="sidebar">
+      <ul className="sidebar__generic">
+        <li className="inbox" data-testid="inbox">
+          <span>
+            <FaInbox />
+          </span>
+          <span>Inbox</span>
+        </li>
+        <li className="today" data-testid="today">
+          <span>
+            <FaRegCalendar />
+          </span>
+          <span>Today</span>
+        </li>
+        <li className="next_7" data-testid="next_7">
+          <span>
+            <FaRegCalendarAlt />
+          </span>
+          <span>Next 7 Days</span>
+        </li>
+      </ul>
+      <div className="sidebar__middle">
         <span>
-          <FaInbox />
+          <FaChevronDown />
         </span>
-        <span>Inbox</span>
-      </li>
-      <li className="today" data-testid="today">
-        <span>
-          <FaRegCalendar />
-        </span>
-        <span>Today</span>
-      </li>
-      <li className="next_7" data-testid="next_7">
-        <span>
-          <FaRegCalendarAlt />
-        </span>
-        <span>Next 7 Days</span>
-      </li>
-    </ul>
-    <div className="sidebar__middle">
-      <span>
-        <FaChevronDown />
-      </span>
-      <h2>Projects</h2>
+        <h2>Projects</h2>
+      </div>
+
+      <ul className="sidebar__projects">{showProjects && <Projects />}</ul>
     </div>
-
-    <ul className="sidebar__projects">Projects will be here!</ul>
-  </div>
-);
+  );
+};
