82e1409ca27c12e59f0bf71a8726adb676a606b6
DAGIKE <isaac20053@hotmail.com>
2019-09-12 14:27:39 -0400

    modify li to be selected

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -7,42 +7,68 @@ import {
 } from "react-icons/fa";
 import { useSelectedProjectValue } from "../../context";
 import ProjectList from "../ProjectList";
+import AddProject from "../AddProject";
 
 export default () => {
-  const { setSelectedProject } = useSelectedProjectValue;
+  const { setSelectedProject } = useSelectedProjectValue();
   const [active, setActive] = useState("INBOX");
   const [showProjects, setShowProjects] = useState(true);
 
   return (
     <div className="sidebar" data-testid="sidebar">
       <ul className="sidebar__generic">
-        <li className="inbox" data-testid="inbox">
+        <li
+          className={active === "inbox" ? "active" : ""}
+          data-testid="inbox"
+          onClick={() => {
+            setActive("inbox");
+            setSelectedProject("INBOX");
+          }}
+        >
           <span>
             <FaInbox />
           </span>
           <span>Inbox</span>
         </li>
-        <li className="today" data-testid="today">
+        <li
+          className={active === "today" ? "active" : ""}
+          data-testid="today"
+          onClick={() => {
+            setActive("today");
+            setSelectedProject("TODAY");
+          }}
+        >
           <span>
             <FaRegCalendar />
           </span>
           <span>Today</span>
         </li>
-        <li className="next_7" data-testid="next_7">
+        <li
+          className={active === "next_7" ? "active" : ""}
+          data-testid="next_7"
+          onClick={() => {
+            setActive("next_7");
+            setSelectedProject("NEXT_7");
+          }}
+        >
           <span>
             <FaRegCalendarAlt />
           </span>
           <span>Next 7 Days</span>
         </li>
       </ul>
-      <div className="sidebar__middle">
+      <div
+        className="sidebar__middle"
+        onClick={() => setShowProjects(!showProjects)}
+      >
         <span>
-          <FaChevronDown />
+          <FaChevronDown className={!showProjects ? "hidden-projects" : ""} />
         </span>
         <h2>Projects</h2>
       </div>
 
       <ul className="sidebar__projects">{showProjects && <ProjectList />}</ul>
+      {showProjects && <AddProject />}
     </div>
   );
 };
